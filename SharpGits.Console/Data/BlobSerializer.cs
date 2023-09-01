bae705f12a2f9933f070b827d55b8a8391de2685
Stephon Brown <stephonb14@gmail.com>
2023-08-31 23:52:05 -0400

    Updates for init and serializer

--- SharpGits.Console/Data/BlobSerializer.cs
+++ SharpGits.Console/Data/BlobSerializer.cs
@@ -1,16 +1,33 @@
+using System.Text;
 using SharpGits.Console.GitObjects;
 
 namespace SharpGits.Console.Data;
 
 public class BlobSerializer
 {
+    private readonly byte[] nullByte = { 0x0 };
+    private readonly byte[] blobAscii = Encoding.ASCII.GetBytes("blob ");
     public byte[] Serialize(Blob blob)
     {
-        throw new NotImplementedException();
+        if (blob == null)
+        {
+            throw new ArgumentNullException(nameof(blob), "Blob to serialize cannot be null");
+        }
+        var content = new List<byte>();
+        var blobLengthAscii = Encoding.ASCII.GetBytes(blob.Content.Length.ToString());
+        content.AddRange(blobAscii);
+        content.AddRange(blobLengthAscii);
+        content.AddRange(nullByte);
+        content.AddRange(blob.Content);
+        return content.ToArray();
     }
 
     public Blob Deserialize(byte[] blobBytes)
     {
-        throw new NotImplementedException();
+        //This skips all header information until the null byte and skips past the null byte
+        return new Blob()
+        {
+            Content = blobBytes.SkipWhile(x => x != 0x0).Skip(1).ToArray(),
+        };
     }
 }
--- SharpGits.Console/Data/Database.cs
+++ SharpGits.Console/Data/Database.cs
@@ -4,7 +4,43 @@ public class Database
 {
     public static void Init(string workspacePath)
     {
-        // TODO Create git database directory and appropriate subdirs.
-        throw new NotImplementedException();
+        var workingDirectory =
+            string.IsNullOrWhiteSpace(workspacePath) ? 
+                Directory.GetCurrentDirectory() : workspacePath;
+        var dotGitDirectory = Path.Combine(workspacePath, ".git");
+        var refsDirectory = Path.Combine(dotGitDirectory, "refs");
+        var objectsDirectory = Path.Combine(dotGitDirectory, "objects");
+        var headFile = Path.Combine(dotGitDirectory, "HEAD");
+
+        try
+        {
+            if (!Directory.Exists(workspacePath))
+            {
+                Directory.CreateDirectory(workingDirectory);
+            }
+            if (!Directory.Exists(dotGitDirectory))
+            {
+                Directory.CreateDirectory(dotGitDirectory);
+            }
+            if (!Directory.Exists(refsDirectory))
+            {
+                Directory.CreateDirectory(refsDirectory);
+            }
+            if (!Directory.Exists(objectsDirectory))
+            {
+                Directory.CreateDirectory(objectsDirectory);
+            }
+            if(!File.Exists(headFile))
+            {
+                File.WriteAllText(headFile,"ref: refs/heads/main\n");
+            }
+        }
+        catch (Exception e)
+        {
+            System.Console.WriteLine(e);
+            throw;
+        }
+
     }
+    
 }
